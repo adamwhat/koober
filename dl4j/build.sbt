6af1f07a86b3994792dfcac25c6d2194eccd4402
ec587 <ec587@cornell.edu>
2017-05-04 19:45:47 -0400

    stand alone model with imported dataset

--- dl4j/build.sbt
+++ dl4j/build.sbt
@@ -1,7 +1,9 @@
 name := "dl4j"
 
 libraryDependencies ++= Seq(
+  "org.apache.predictionio"    %% "apache-predictionio-core" % "0.10.0-incubating" % "provided",
+  "org.apache.spark"  %% "spark-core"   % "1.3.0"           % "provided",
+  "org.apache.spark"  %% "spark-mllib"  % "1.3.0"           % "provided",
   "org.deeplearning4j" % "deeplearning4j-core" % "0.8.0",
-  "org.nd4j" % "nd4j-native-platform" % "0.8.0",
-  "org.slf4j" % "slf4j-simple" % "1.7.21"
+  "org.nd4j" % "nd4j-native-platform" % "0.8.0"
 )
--- dl4j/src/main/scala/DL4J.scala
+++ dl4j/src/main/scala/DL4J.scala
@@ -1,11 +1,36 @@
+//import org.deeplearning4j.datasets.iterator.DataSetIterator;
+//import org.deeplearning4j.datasets.iterator.impl.IrisDataSetIterator;
+import org.deeplearning4j.eval.Evaluation;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+//import org.deeplearning4j.nn.conf.layers.Layer
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.deeplearning4j.nn.conf.layers.RnnOutputLayer;
+import org.deeplearning4j.nn.conf.Updater;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.nn.params.DefaultParamInitializer;
+import org.deeplearning4j.nn.weights.WeightInit;
+import org.deeplearning4j.nn.api.OptimizationAlgorithm;
+import org.deeplearning4j.optimize.api.IterationListener;
+import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.lossfunctions.LossFunctions.LossFunction.MSE;
+import scala.io._
 
 object DL4J extends App {
 
-  /*
-  //val Nd4j_MAX_SLICES_TO_PRINT = 10
-  //val Nd4j_MAX_ELEMENTS_PER_SLICE = 10
+  // Read in training data
+  val f = "features.csv"
+  val l = "labels.csv"
+  val featuresArray = Source.fromFile(f).getLines().map(_.split(",").map(_.trim.toDouble)).toArray
+  val labelsArray = Source.fromFile(l).getLines().map(_.trim.toDouble).toArray
 
-  val conf: MultiLayerConfiguration = new NeuralNetConfiguration.Builder()
+  val conf : MultiLayerConfiguration
+  = new NeuralNetConfiguration.Builder()
     .seed(12345)
     .iterations(20)
     .learningRate(0.1)
@@ -13,21 +38,22 @@ object DL4J extends App {
     .weightInit(WeightInit.XAVIER)
     .updater(Updater.NESTEROVS).momentum(0.9)
     .list()
-    .layer(0, new DenseLayer.Builder().nIn(5).nOut(3)
-      .activation(Activation.TANH)
-      .build())
-    .layer(1, new OutputLayer.Builder(LossFunction.RMSE_XENT)
+    .layer(0, new DenseLayer.Builder().nIn(9).nOut(20)
+      .activation(Activation.TANH).build())
+    .layer(1, new RnnOutputLayer.Builder(MSE)
       .activation(Activation.IDENTITY)
-      .nIn(3).nOut(1).build())
-    .backprop(true).pretrain(false)
-    .build()
-
-  val model: MultiLayerNetwork = new MultiLayerNetwork(conf)
+      .nIn(20).nOut(10000).build())
+    .backprop(true).pretrain(false).build()
+  conf.setPretrain(true)
+  val model : MultiLayerNetwork = new MultiLayerNetwork(conf)
   model.init()
   model.setListeners(new ScoreIterationListener(2))
 
-  model.fit()
-  */
-  MultiLayerNetworkExternalErrors.main(args)
+  var dataset = new DataSet(Nd4j.create(featuresArray), Nd4j.create(labelsArray))
+  var inputINDArray = Nd4j.create(featuresArray)
+
+  model.fit(inputINDArray, Nd4j.create(labelsArray))
+
+//  MultiLayerNetworkExternalErrors.main(args)
 
 }
