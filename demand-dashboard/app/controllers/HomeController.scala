f87e4854ab2e45e6e2904d867e7c4694255aae43
Siyuan Wang <wsylafa@gmail.com>
2017-05-04 19:01:30 -0400

    Import all new changes from dashboardv2_pr_20170501 branch.

--- demand-dashboard/app/controllers/HomeController.scala
+++ demand-dashboard/app/controllers/HomeController.scala
@@ -52,94 +52,35 @@ class HomeController @Inject() (configuration: Configuration, predictionIO: Pred
     Ok(views.html.dashboardPrediction(mapboxAccessToken))
   }
 
-  //    request =>
-  //    val eventTime = (request.body \ "eventTime").as[DateTime]
-  //    val lat = (request.body \ "lat").as[Double]
-  //    val lng = (request.body \ "lng").as[Double]
-  //    val temperature = (request.body \ "temperature").as[Double]
-  //    val clear = (request.body \ "clear").as[Int]
-  //    val fog = (request.body \ "fog").as[Int]
-  //    val rain = (request.body \ "rain").as[Int]
-  //    val snow = (request.body \ "snow").as[Int]
-  //    val hail = (request.body \ "hail").as[Int]
-  //    val thunder = (request.body \ "thunder").as[Int]
-  //    val tornado = (request.body \ "tornado").as[Int]
-  //    val heat = (request.body \ "heat").as[Double]
-  //    val windchill = (request.body \ "windchill").as[Double]
-  //    val precipitation = (request.body \ "precipitation").as[Double]
-
   // todo: input checking and error handling
   def predict(eventTime: String, lat: Double, lng: Double, temperature: Double,
               clear: Int, fog: Int, rain: Int, snow: Int, hail: Int, thunder: Int, tornado: Int) = Action.async {
 
     var lngLatArray = makeCluster(lat, lng)
-//    var resultSeq = new ListBuffer[Future[JsValue]](lngLatArray(0).length)
 
     val resultSeq = (0 to (lngLatArray(0).length - 1)).map(i => {
-        var query = Json.obj(
-          "eventTime" -> eventTime,
-          "lat" -> lngLatArray(0)(i),
-          "lng" -> lngLatArray(1)(i),
-          "temperature" -> temperature,
-          "clear" -> clear,
-          "fog" -> fog,
-          "rain" -> rain,
-          "snow" -> snow,
-          "hail" -> hail,
-          "thunder" -> thunder,
-          "tornado" -> tornado
-        )
-        print("hi")
-        print(query)
-        var prediction = predictionIO.predict(query)
-        prediction.map{json => toGeoJson2(json, lngLatArray(0)(i), lngLatArray(1)(i), i) }
+      var query = Json.obj(
+        "eventTime" -> eventTime,
+        "lat" -> lngLatArray(0)(i),
+        "lng" -> lngLatArray(1)(i),
+        "temperature" -> temperature,
+        "clear" -> clear,
+        "fog" -> fog,
+        "rain" -> rain,
+        "snow" -> snow,
+        "hail" -> hail,
+        "thunder" -> thunder,
+        "tornado" -> tornado
+      )
+      var prediction = predictionIO.predict(query)
+      prediction.map { json => toGeoJson2(json, lngLatArray(0)(i), lngLatArray(1)(i), i) }
 
     })
 
     var result = Future.sequence(resultSeq)
-    result.map{r => Ok(toGeoJsonCollection(r))}
-
-//    for ( i <- 0 to (lngLatArray(0).length - 1)) {
-//      var query = Json.obj(
-//        "eventTime" -> eventTime,
-//        "lat" -> lngLatArray(0)(i),
-//        "lng" -> lngLatArray(1)(i),
-//        "temperature" -> temperature,
-//        "clear" -> clear,
-//        "fog" -> fog,
-//        "rain" -> rain,
-//        "snow" -> snow,
-//        "hail" -> hail,
-//        "thunder" -> thunder,
-//        "tornado" -> tornado
-//      )
-//      print("hi")
-//      print(query)
-//      var prediction = predictionIO.predict(query)
-////      predictionIO.predict(query).map { json =>
-////        Ok(toGeoJson2(json, lat, lng))
-////      }
-//      resultSeq(i) = prediction.map{json => toGeoJson2(json, lngLatArray(0)(i), lngLatArray(1)(i), i)}
-//
-//    }
-//    var result = Future.sequence(resultSeq)
-//    resuls.map{r => Ok(toGeoJsonCollection(r))}
-
-
-//    predictionIO.predict(eventTime, lat, lng, temperature, clear,
-//      fog, rain, snow, hail, thunder, tornado, heat, windchill, precipitation)
-//      .map { json =>
-//        Ok(toGeoJson2(json, lat, lng))
-//      }
+    result.map { r => Ok(toGeoJsonCollection(r))
+    }
   }
-
-//  def fakePredict = Action {
-//    Ok(
-//      Json.obj(
-//        "demand" -> Random.nextInt()
-//      )
-//    )
-//  }
 
   private def toGeoJson2(json: JsValue, lat: Double, lon: Double, id: Int) = {
     var demand = (json \ "demand").as[Double]
@@ -172,7 +113,6 @@ class HomeController @Inject() (configuration: Configuration, predictionIO: Pred
   }
 
   private def toGeoJsonCollection(demands: IndexedSeq[JsValue]) = {
-    print(demands)
     Json.obj(
       "type" -> "FeatureCollection",
       "features" -> demands
--- demand-dashboard/public/prediction.js
+++ demand-dashboard/public/prediction.js
@@ -1,24 +1,5 @@
 $(function() {
   // todo: apply code to demand popup on map
-//  $("#demand-form").submit(function(event) {
-//    event.preventDefault();
-//    var datetime = $("#date-select").val() + " " + $("#time-select").val();
-//    var eventTime = new Date(datetime);
-//    console.log(eventTime);
-//
-//    var postConfig = {
-//      url: $("#demand-form").attr("action"),
-//      data: JSON.stringify({
-//        eventTime: eventTime
-//      }),
-//      contentType : 'application/json',
-//      success: function(data) {
-//        $("#demand-value").text(data.demand);
-//      }
-//    };
-//
-//    $.post(postConfig);
-//  });
 
   // todo: use a default center based on data or get user's location
   var mapCenter = [-73.9440917, 40.7682802];
@@ -87,7 +68,6 @@ $(function() {
     predictionMap.addSource("demand", {
          type: "geojson",
          data: "/predict?" + buildQueryString(lat, lng)
-//         data: "/demand?lng=" + lng + "&lat=" + lat
     });
     predictionMap.addLayer({
           "id": "prediction",
@@ -112,87 +92,8 @@ $(function() {
             'circle-opacity' : 0.8
           }
       });
-
-//    var latLngArray = makeCluster(lat, lng)
-//    var features = []
-//    var j = 0
-//    for (i = 0; i < latLngArray[0].length; i ++){
-//        var query = buildQueryJson(latLngArray[0][j], latLngArray[1][j])
-//        //console.log(latLngArray)
-//        $.ajax({
-//         url: "http://localhost:5000/queries.json",
-//         type: 'POST',
-//         dataType: 'json',
-//         contentType: 'application/json',
-//          data: query,
-//         success: function (d) {
-//            //console.log(d)
-//            features.push({
-//                "type": "Feature",
-//                "properties": {
-//                    "Primary ID": j,
-//                    "demand": d["demand"]
-//                  },
-//                  "geometry":{
-//                    "type": "Point",
-//                    "coordinates": [latLngArray[1][j], latLngArray[0][j]]
-//                  }
-//            });
-//        if (j == 0){
-//            new mapboxgl.Popup()
-//            .setLngLat(data.lngLat)
-//            .setHTML('<h2>Demand:' + d["demand"] + '</h2>')
-//            .addTo(predictionMap);
-//        }
-//        if (j == latLngArray[0].length-1) {
-//            //console.log(features)
-//            predictionMap.addSource("demand", {
-//                type: "geojson",
-//                data: {"type": "FeatureCollection",
-//                       "features": features
-//                    }
-//              });
-//            predictionMap.addLayer({
-//                  "id": "prediction",
-//                  "type": "circle",
-//                  "source": "demand",
-//                  "paint": {
-//                    "circle-color": {
-//                        property: 'demand',
-//                        type: 'exponential',
-//                        stops: [
-//                              [10.0, '#fee5d9'],
-//                              [20.0, '#fcae91'],
-//                              [30.0, '#fb6a4a'],
-//                              [40.0, '#de2d26'],
-//                              [50.0, '#a50f15']
-//                            ]
-//                    },
-//                    "circle-radius": {
-//                        'base': 1.75,
-//                        'stops': [[12, 3], [22, 180]]
-//                    },
-//                    'circle-opacity' : 0.8
-//                  }
-//              });
-//        }
-//        j += 1;
-//
-//       }, error: function(){
-//           alert("Cannot get data");
-//         }
-//        });
-//
-//    }
-
-    //todo: make call to predict
-
-//    predictionMap.setFilter('prediction',
-//        ['>=', 'properties.demand', '9.0'],
-//        ['<', 'properties.demand', '10.0']
-//    )
-
   });
+
   function makeCluster(lat, lng) {
     lats = [lat];
     lngs = [lng];
@@ -205,8 +106,6 @@ $(function() {
     result = {}
     var datetime = date + " " + time;
     var eventTime = new Date(datetime);
-    //console.log(datetime);
-    //console.log(eventTime.toISOString());
     result = {
     "eventTime":eventTime.toISOString(),
     "lat":lat,
@@ -220,8 +119,6 @@ $(function() {
     "thunder":weatherArray[5],
     "tornado":weatherArray[6]
     }
-
-    //console.log(JSON.stringify(result))
     return JSON.stringify(result)
   }
 
